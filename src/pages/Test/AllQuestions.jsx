7734281790019b6c1d1e20bbe2d3783552656643
Mayank Shukla <mayank.m.shukla@oracle.com>
2024-10-01 07:01:42 +0530

    All questions

---
+++ src/pages/Test/AllQuestions.jsx
@@ -0,0 +1,55 @@
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import React from "react";
+
+const AllQuestions = ({questions=[], setAllQuestions}) => {
+
+	function returnOption(idx) {
+		if (idx === 0)
+			return 'A';
+		else if (idx === 1)
+			return 'B';
+		else if (idx === 2)
+			return 'C';
+		else return 'D';
+	}
+
+	return (
+		<div className="h-screen overflow-y-scroll">
+				<XMarkIcon className="fixed h-8 right-3 top-2 bg-secondary cursor-pointer" onClick={() => setAllQuestions(false)}/>
+				<div className="container mt-10">
+					
+					{
+						questions.map((question, idx) => (
+							<>
+								<div key={idx} className="flex gap-4 my-2 border-t-2">
+									<h1>{idx + 1}.</h1>
+									<div>
+										{
+											question.quetionDescription.split('\n').map((line, qId) => (
+													<p key={qId} className={`dark:text-gray-200 text-gray-800 `}>
+														{line}
+													</p>
+											))
+										}
+									</div>
+								</div>
+								<div className="flex flex-col mt-10 gap-2 w-full">
+									{
+										question.options.map((option, oidx) => (
+											<label key={oidx} className="inline-flex gap-4 px-2 py-1 items-center">
+												
+												{ returnOption(oidx)}. {option}
+											</label>
+										))
+									}
+								</div>
+							</>
+						))
+					}
+					
+				</div>
+		</div>
+	)
+}
+
+export default AllQuestions;
