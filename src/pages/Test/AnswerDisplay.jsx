36ef7f1b893ff6c3c1b4ca1e3a53c4312bd314f1
mentorsgyan <mentorsgyan@gmail.com>
2024-10-05 14:39:07 +0530

    RAnk list
    * 15 Aug changes
    
    * Removed banner
    
    * Premium e-library draft
    
    * Payment page
    
    * User page update
    
    * Updated profile section and hero
    
    * Light Dark mode and premium ebooks
    
    * Minor fixes
    
    * Minor fixes
    
    * Minor fix
    
    * Brahmastra elibrary update
    
    * Dynamic mentorship loading
    
    * Admin side enhancement
    
    * Minor fix
    
    * Test series push
    
    * Minor Fix
    
    * Minor UI fix
    
    * Minor UI fix
    
    * Minor fix
    
    * Minor fixes
    
    * UI enhancement
    
    * Duplicate data saving fix
    
    * Admin side UI changes
    
    * Minor fix
    
    * Test onboarding
    
    * Minor changes
    
    * Minor changes
    
    * Minor enhancement
    
    * language DD fix
    
    * Test build
    
    * Minor change
    
    * minor fix
    
    * Enhancements
    
    * All questions
    
    * Latest test upload
    
    * Minor fix
    
    * Answer key changes
    
    * Answer key release
    
    * Minor fix
    
    * Rwsponse display changes
    
    * Removed CG gov logo
    
    * Minor fixes before test
    
    * Test 2 live
    
    * Minor fixes
    
    * Test 2 answerkey and rl
    
    ---------
    
    Co-authored-by: Mayank Shukla <24ms.yaml@gmail.com>
    Co-authored-by: Mayank Shukla <mayank.m.shukla@oracle.com>

--- src/pages/Test/AnswerDisplay.jsx
+++ src/pages/Test/AnswerDisplay.jsx
@@ -7,19 +7,19 @@ import { LanguageIcon } from "@heroicons/react/24/outline";
 const AnswerDisplay = () => {
 	
 	const [searchParams] = useSearchParams();
+
 	const [english, setEnglish] = useState(false);
-	const [hindiQuestions, setHindiQuestions] = useState([]);
-	const [englishQuestions, setEnglishQuestions] = useState([]);
-	const questions = english ? englishQuestions : hindiQuestions;
-	const [totalCorrect, setTotalCorrect] = useState(0);
-	const [totalIncorrect, setTotalIncorrect] = useState(0);
-	const [totalMarks, setTotalMarks] = useState(0);
+	
+	const [testResultData, setTestResultData] = useState({
+		score: {correct: 0, incorrect: 0, totalMarks: 0},
+		questions: {englishAns: [], hindiAns: []}
+	})
+	
+	const questions = english ? testResultData.questions.englishAns : testResultData.questions.hindiAns;
 
 	const phoneNumber = searchParams.get('phoneNumber');
 	const testId = searchParams.get("testId");
 	const testNumber = searchParams.get("test");
-
-	const [userAnsers, setUserAnswers] = useState(undefined);
 
 	function returnOption(idx) {
 		if (idx === 0)
@@ -35,14 +35,8 @@ const AnswerDisplay = () => {
 		const data = {
 			phoneNumber, testId, testNumber
 		}
-		const response = await axios.post(BACKEND_API + "/test-series/fetchAnswers", data);
-		if (response.status === 200) {
-			setTotalCorrect(response.data.score.correct);
-			setTotalIncorrect(response.data.score.incorrect);
-			setTotalMarks(response.data.score.totalMarks);
-		}
-		setEnglishQuestions(response.data.questions.englishAns);
-		setHindiQuestions(response.data.questions.hindiAns);
+		const response = await axios.post(BACKEND_API + "/test-series/testResults", data);
+		setTestResultData(response.data);
 	}
 
 	useEffect(() => {
@@ -63,21 +57,28 @@ const AnswerDisplay = () => {
 						<h1 className="font-bold dark:border-gray-700 bg-green-200 p-2 border border-gray-200">Correct (+2)</h1>
 						<h1 className="font-bold dark:border-gray-700 bg-red-200 p-2 border border-gray-200">Incorrect(-2/3)</h1>
 						<h1 className="font-bold dark:border-gray-700 bg-blue-100 p-2 border border-gray-200">Total Attempted</h1>
-						<p className="p-2 border dark:bg-gray-100 dark:border-gray-700 border-gray-200">{totalCorrect}</p>
-						<p className="p-2 border dark:bg-gray-100 dark:border-gray-700 border-gray-200">{totalIncorrect}</p>
-						<p className="p-2 border dark:bg-gray-100 dark:border-gray-700 border-gray-200">{totalIncorrect + totalCorrect}</p>
-						<h1 className="col-span-3 bg-blue-100 p-2 border-2">Marks Obtained: {totalMarks}</h1>
+						<p className="p-2 border dark:bg-gray-100 dark:border-gray-700 border-gray-200">{testResultData.score.correct}</p>
+						<p className="p-2 border dark:bg-gray-100 dark:border-gray-700 border-gray-200">{testResultData.score.incorrect}</p>
+						<p className="p-2 border dark:bg-gray-100 dark:border-gray-700 border-gray-200">{testResultData.score.incorrect + testResultData.score.correct}</p>
+						<h1 className="col-span-3 bg-blue-100 p-2 border-2">Marks Obtained: {testResultData.score.totalMarks}</h1>
 					</div>
 					<div className="grid md:hidden grid-cols-2 items-center justify-center text-center border-2">
 						<h1 className="font-bold dark:border-gray-700 bg-green-200 p-2 border border-gray-200">Correct (+2)</h1>
 						<h1 className="font-bold dark:border-gray-700 bg-red-200 p-2 border border-gray-200">Incorrect(-2/3)</h1>
-						<p className="p-2 border dark:border-gray-700 dark:bg-gray-100 border-gray-200">{totalCorrect}</p>
-						<p className="p-2 border dark:border-gray-700 dark:bg-gray-100 border-gray-200">{totalIncorrect}</p>
+						<p className="p-2 border dark:border-gray-700 dark:bg-gray-100 border-gray-200">{testResultData.score.correct}</p>
+						<p className="p-2 border dark:border-gray-700 dark:bg-gray-100 border-gray-200">{testResultData.score.incorrect}</p>
 						<h1 className="font-bold dark:border-gray-700 bg-blue-100 p-2 border border-gray-200">Total Attempted</h1>
-						<p className="p-2 border dark:border-gray-700 bg-blue-100 border-gray-200">{totalIncorrect + totalCorrect}</p>
-						<h1 className="col-span-2 bg-blue-100 p-2 border-t-2 border-gray-300">Marks Obtained: {totalMarks}</h1>
+						<p className="p-2 border dark:border-gray-700 bg-blue-100 border-gray-200">{testResultData.score.incorrect + testResultData.score.correct}</p>
+						<h1 className="col-span-2 bg-blue-100 p-2 border-t-2 border-gray-300">Marks Obtained: {testResultData.score.totalMarks}</h1>
 					</div>
-				</div> 
+				</div>
+
+				{/* RankList */}
+				{
+					testId !== "0110" && <div className="container mt-10">
+							<a href={`/test/ranks?phoneNumber=${phoneNumber}&testId=${testId}&test=${testNumber}`} className="text-center font-bold text-xl animate-pulse text-secondary">Click here for ranklist!</a>
+						</div>
+				}
 				
 				<div className="container mt-10">
 					<h1 className="text-center font-bold text-3xl">Answer Key</h1>
@@ -100,16 +101,12 @@ const AnswerDisplay = () => {
 								<div key={(idx+1) * 31} className="flex flex-col mt-10 gap-2 w-full">
 									{
 										question?.options?.map((option, oidx) => {
-											const qId = hindiQuestions[idx].questionId;
-											const markedCorrect = userAnsers !== undefined ? userAnsers[qId] === hindiQuestions[idx].options[oidx] || userAnsers[qId] === englishQuestions[idx].options[oidx] : false;
 											const correctAnswer = option === question.correct;
 											return (
 												<label key={oidx} className="inline-flex gap-4 px-2 py-1 items-center">
 													
 													<p className={`${correctAnswer ? 'bg-green-300 dark:bg-green-600/70 rounded-md' : ''} px-2 py-1`}>{ returnOption(oidx)}. {option}</p>
-													{
-														markedCorrect && <p>✔</p>
-													}
+													
 												</label>
 										)})
 									}
