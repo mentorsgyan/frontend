c65041778b4b9b86814c51bb3e3eef84d8f0470a
Mayank Shukla <mayank.m.shukla@oracle.com>
2024-10-08 19:31:35 +0530

    Test 4 admin

--- src/pages/Admin/AdminControlls.jsx
+++ src/pages/Admin/AdminControlls.jsx
@@ -14,8 +14,8 @@ const AdminControls = () => {
         <div className="font-mukta">
 			<AdminNavbar />
 
-			<button className="mt-32 mx-20 bg-secondary w-fit p-5" onClick={() => {navigate("/test/start/3?phoneNumber=9179263530", {state: {valid: true}})}}>Go to test 1</button>
-			<button className="mt-32 mx-20 bg-secondary w-fit p-5" onClick={() => {navigate("/test/result?phoneNumber=9179263530&testId=0610&test=3", {state: {valid: true}})}}>Go to test 1 Answer Key</button>
+			<button className="mt-32 mx-20 bg-secondary w-fit p-5" onClick={() => {navigate("/test/start/4?phoneNumber=9179263530", {state: {valid: true}})}}>Go to test 1</button>
+			<button className="mt-32 mx-20 bg-secondary w-fit p-5" onClick={() => {navigate("/test/result?phoneNumber=9179263530&testId=0610&test=4", {state: {valid: true}})}}>Go to test 1 Answer Key</button>
 
 			{/* Mentorship updating section */}
 			<AdminMentorship />
