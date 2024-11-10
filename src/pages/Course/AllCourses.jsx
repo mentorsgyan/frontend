fc451687b5ac0334df66611c05ac00218eeec5e0
misky0310 <misky0310@gmail.com>
2024-11-10 12:05:26 +0530

    Made minor changes after review

--- src/pages/Course/AllCourses.jsx
+++ src/pages/Course/AllCourses.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import Image from "../../../../Courses by MentorsGyan.png";
-import Line from "../../../../Vector 1.png";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
+import { FaPeopleGroup } from "react-icons/fa6";
 
 const AllCourses = () => {
-
   //sample data for CourseCardInfoDto
- // the logic of useEffect to fetch all the courses will be implemented here and the state will be updated appropriately   
-  
- let CourseCardInfoDtos = [
+  // the logic of useEffect to fetch all the courses will be implemented here and the state will be updated appropriately
+
+  let CourseCardInfoDtos = [
     {
       courseName: "Name1",
       price: "2000",
@@ -37,7 +35,7 @@ const AllCourses = () => {
     {
       courseName: "Mathematics for MAINS",
       price: "3000",
-    //   offerPrice: "2000",
+      //   offerPrice: "2000",
       headline:
         "One liner course summary provided when an API call is made to the backend is it is ibig it will look like thi , can you handle this case? aslo will iy overlgov",
       courseThumbnail: "URL of the course thumbnail",
@@ -61,17 +59,20 @@ const AllCourses = () => {
       enrollmentCount: 240, //display only if count > 0
     },
   ];
-  
+
   return (
     <div className="bg-gray-100 dark:bg-gray-800 dark:text-white">
-        <Navbar sticky={true}/>
+      <Navbar sticky={true} />
       {/* <h1 className="text-center font-inter font-semibold text-[#fd661f] text-5xl pt-16 pb-3">Courses by <span className="text-black">Mentors</span>Gyan</h1> */}
-      
-      <div className="flex flex-col xl:w-1/3 px-4 items-center justify-center mx-auto pt-28 relative">
-        <img src={Image} className="pb-10" alt="" />
-        <img src={Line} className="absolute w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/3 right-8 md:right-40 lg:right-64 xl:right-12 bottom-7"></img>
+
+      {/* Title - Courses by MentorsGyan */}
+      <div className="px-4 text-center pt-28">
+        <h1 className="text-[#fd661f] text-xl md:text-3xl font-semibold font-inter">
+          Courses by{" "}
+          <span className="dark:text-gray-100 text-xl md:text-3xl text-gray-900">Mentors</span>Gyan
+        </h1>
       </div>
-      
+
       <div
         id="header-component"
         className="relative dark:bg-gray-800 dark:text-white px-6 pt-14 pb-10 w-full grid grid-cols-1 xl:grid-cols-4 md:grid-cols-2 gap-8 bg-gray-100"
@@ -85,36 +86,44 @@ const AllCourses = () => {
 };
 
 const CourseCard = ({ course }) => {
+  const {
+    courseName,
+    courseThumbnail,
+    headline,
+    offerPrice,
+    price,
+    enrollmentCount,
+  } = course;
 
-    const {courseName,courseThumbnail,headline,offerPrice,price,enrollmentCount}=course;
+  const navigate = useNavigate();
 
-    const navigate=useNavigate();
-    
-    const handleThumbnailClick = (courseName) => {
-        navigate(`/course/${courseName}`)
-    }
-  
-    return (
-    <div className="bg-white rounded-3xl shadow-lg dark:shadow-slate-900 min-h-[500px] relative dark:bg-gray-800 dark:text-white">
-        
-        {/* section which shows the no of enrolled students */}
-        {enrollmentCount>0 && (
-            <div className="bg-zinc-100 dark:bg-gray-700 w-3/4 font-light absolute h-10 top-[25%] md:top-[28%] left-[50%] -translate-x-[50%] rounded-full flex justify-center items-center gap-2">
-                ..... + <span className="font-inter font-semibold">{enrollmentCount-5}</span> Students
-            </div>
-        )}
+  const handleThumbnailClick = (courseName) => {
+    navigate(`/course/${courseName}`);
+  };
 
-        {/* If a card has more content and exceeds 500px, it will grow to fit its content, but for less data , the card will atleast be 500px so that the visual uniformity is maintained */}
-      
+  return (
+    <div className="bg-white rounded-3xl shadow-lg dark:shadow-slate-900 min-h-[450px] relative dark:bg-gray-800 dark:text-white">
+      {/* section which shows the no of enrolled students */}
+      {enrollmentCount > 0 && (
+        <div className="bg-zinc-100 dark:bg-gray-700 w-3/4 font-light absolute h-10 top-[25%] md:top-[28%] left-[50%] -translate-x-[50%] rounded-full flex justify-center items-center gap-2">
+          <span className="font-inter font-semibold flex gap-2 items-center">
+            <FaPeopleGroup className="w-5 h-5" />
+            {enrollmentCount}
+          </span>{" "}
+          Students
+        </div>
+      )}
+
+      {/* If a card has more content and exceeds 500px, it will grow to fit its content, but for less data , the card will atleast be 500px so that the visual uniformity is maintained */}
+
       {/* image / thumbnail */}
-      <div onClick={() => handleThumbnailClick(courseName)} className="w-full cursor-pointer h-40 bg-cyan-300 object-cover object-center rounded-tl-3xl rounded-tr-3xl"></div>
+      <div
+        onClick={() => handleThumbnailClick(courseName)}
+        className="w-full cursor-pointer h-40 bg-cyan-300 object-cover object-center rounded-tl-3xl rounded-tr-3xl"
+      ></div>
 
       {/* date and title */}
       <div className="px-7 pt-16 flex flex-col gap-4">
-        
-        {/* Replace with the dates/duration of the course if passed  */}
-        <span className="text-[#777795] text-xs">1-28 July 2022</span> 
-        
         <h3 className="text-[#0B7077] dark:text-[#4bb8c0] font-semibold text-lg">
           {courseName}
         </h3>
@@ -123,19 +132,21 @@ const CourseCard = ({ course }) => {
       {/* headline */}
       <div className="px-7 pt-10 h-28  overflow-hidden">
         {/* if the headline is too big, truncate it , for now keep the harcoded value as 80 */}
-        <span className="font-light tracking-tighter ">{headline.length>100 ? `${headline.slice(0,80)} ...` : headline}</span>
+        <span className="font-light tracking-tighter ">
+          {headline.length > 100 ? `${headline.slice(0, 80)} ...` : headline}
+        </span>
       </div>
 
       {/* price and enroll */}
       <div className="px-7 pt-4 pb-10 w-full flex flex-col md:flex-row justify-between items-center">
         <div className="flex space-x-2 justify-center md:justify-start md:pb-0 pb-6 md:items-center w-1/3">
-          {course.offerPrice? (
+          {course.offerPrice ? (
             <>
-                <span className="text-[#fd661f] font-bold">&#8377;{offerPrice}</span>
-                <span className="text-[#777795] line-through">{price}</span>
+              <span className="text-[#fd661f] font-bold">₹{offerPrice}</span>
+              <span className="text-[#777795] line-through">{price}</span>
             </>
           ) : (
-                <span className="text-[#fd661f] font-bold">&#8377;{price}</span>
+            <span className="text-[#fd661f] font-bold">₹{price}</span>
           )}
         </div>
 
